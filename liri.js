b725a43d0fc808835721a811c1084b624b22daec
Dylan Marini <dylan.marini@g.austincc.edu>
2018-08-21 21:11:19 -0500

    final product

--- liri.js
+++ liri.js
@@ -59,54 +59,82 @@ if (process.argv[2] === "spotify-this-song") {
 }
 
 function band(input){
+
+    var divider = "\n------------------------------------------------------------\n\n";
+
     bandsintown
   .getArtistEventList(input.trim())
   .then(function(events) {
-    console.log(events[0].venue.name);
 
-    //place
-    console.log(events[0].venue.place);
-    //city
-    console.log(events[0].venue.city);
-    //country
-    console.log(events[0].venue.country);
 
-    console.log(events[0].title);
+    var showData = [
+        "Venue Name: " + events[0].venue.name,
+        "Location: " + events[0].venue.place,
+        "City: " + events[0].venue.city,
+        "Country: " + events[0].venue.country,
+        "Title: " + events[0].title,
+        "Formatted Location: " + events[0].formatted_location,
+        "Date: " + moment(events[0].datetime).format('L')
+      ].join("\n\n");
 
-    console.log(events[0].formatted_location);
+      // Append showData and the divider to log.txt, print showData to the console
+      fs.appendFile("log.txt", showData + divider, function(err) {
+        if (err) throw err;
+        console.log(showData);
+      });
 
-    console.log(moment(events[0].datetime).format('L'));
   });
 }
 
 function movie(input){
+
+    var divider = "\n------------------------------------------------------------\n\n";
 
     request("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
 
         if (!error && response.statusCode === 200) {
 
 
-            console.log(JSON.parse(body).Title);
-            console.log(JSON.parse(body).Year);
-            console.log(JSON.parse(body).imdbRating);
-            console.log(JSON.parse(body).Ratings[1].Value);
-            console.log(JSON.parse(body).Country);
-            console.log(JSON.parse(body).Language);
-            console.log(JSON.parse(body).Plot);
-            console.log(JSON.parse(body).Actors);
+            var showData = [
+                "Movie Title: " + JSON.parse(body).Title,
+                "Year: " + JSON.parse(body).Year,
+                "IMDB Rating: " + JSON.parse(body).imdbRating,
+                "Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value,
+                "Country: " + JSON.parse(body).Country,
+                "Language: " + JSON.parse(body).Language,
+                "Plot: " + JSON.parse(body).Plot,
+                "Actors: " + JSON.parse(body).Actors
+              ].join("\n\n");
+
+              fs.appendFile("log.txt", showData + divider, function(err) {
+                if (err) throw err;
+                console.log(showData);
+              });
+
         }
     });
 }
 
 function newSong(input){
+
+    var divider = "\n------------------------------------------------------------\n\n";
+
     spotify.search({ type: 'track', query: input }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
 
-        console.log(data.tracks.items[0].album.external_urls.spotify);
-        console.log(data.tracks.items[0].album.name);
-        console.log(data.tracks.items[0].album.artists[0].name);
-        console.log(input.trim());
+        var showData = [
+            "Artist: " + data.tracks.items[0].album.artists[0].name,
+            "Song Name: " + input.trim(),
+            "Album Name: " + data.tracks.items[0].album.name,
+            "Spotify Url To Listen: " + data.tracks.items[0].album.external_urls.spotify
+          ].join("\n\n");
+
+          fs.appendFile("log.txt", showData + divider, function(err) {
+            if (err) throw err;
+            console.log(showData);
+          });
+
     });
 }
